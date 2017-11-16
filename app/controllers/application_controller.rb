576256c57a099255679492f0c56da7873db96054
Hannah Danziger <hdanziger8@gmail.com>
2017-11-16 14:53:26 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,13 +21,13 @@ class ApplicationController < Sinatra::Base
 
 
   get '/account' do
-    @current_user = User.find_by_id(session[:user_id])
-    if @current_user
-      erb :account
-    else
-      erb :error
-    end
+  @current_user = User.find_by_id(session[:user_id])
+  if @current_user
+    erb :account
+  else
+    erb :error
   end
+end
 
   get '/logout' do
     session.clear
