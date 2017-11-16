e4e16a2a0d6a1f6275dbe6ce8a255f6dcd9bfdda
Hannah Danziger <hdanziger8@gmail.com>
2017-11-16 14:05:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find(params[:username])
     session[:user_id] = @user.id
-    redirect to '/account'
+    redirect '/account'
   end
 
   get '/account' do
@@ -22,6 +22,7 @@ class ApplicationController < Sinatra::Base
 
   get '/logout' do
     session.clear
+    redirect '/'
   end
 
 
