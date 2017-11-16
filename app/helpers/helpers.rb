79a9b4fa83d8809d7fee49575eea3cd7949eca53
Hannah Danziger <hdanziger8@gmail.com>
2017-11-16 14:16:07 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,14 @@
 class Helpers
 
-  def current_user
+  def current_user(session)
+    
   end
 
-  def is_logged_in?
+  def is_logged_in?(session)
+    if session.include?(:user_id)
+    else 
+      false 
+    end
+      
   end
 end
